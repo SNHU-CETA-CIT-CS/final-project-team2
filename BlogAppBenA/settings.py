6d2923c8d5a4866ee0063c0143e49a751d0fc04b
benangel03 <benjamin.angel@snhu.edu>
2022-11-18 11:34:19 -0500

    Removing secret key

--- BlogAppBenA/settings.py
+++ BlogAppBenA/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8^7=(=rfpz&_83xm*drj*xw^(gry0%*_s(-wem@ki(rv8)*rt5'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
